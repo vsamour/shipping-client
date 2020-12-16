8f5d0fd743e100665f006f3204d1faec883651ca
vs.amour <salentin.shtanko@gmail.com>
2020-12-16 17:23:22 +0200

    [CORE] Implemented core module as singleton service

--- src/app/@core/core.module.ts
+++ src/app/@core/core.module.ts
@@ -1,12 +1,32 @@
-import { NgModule } from '@angular/core';
+import {ModuleWithProviders, NgModule, Optional, SkipSelf} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {throwIfAlreadyLoaded} from './module-import-guard';
 
+const DATA_SERVICES = [
+  // Place for services
+];
 
+const CORE_PROVIDERS = [
+  ...DATA_SERVICES
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
   ]
 })
-export class CoreModule { }
+export class CoreModule {
+  constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
+    throwIfAlreadyLoaded(parentModule, 'CoreModule');
+  }
+
+  static forRoot(): ModuleWithProviders<CoreModule> {
+    return <ModuleWithProviders<CoreModule>>{
+      ngModule: CoreModule,
+      providers: [
+        ...CORE_PROVIDERS,
+      ],
+    };
+  }
+}
